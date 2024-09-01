763aa227b99fecb07c7c605de03fe5aca7363d7d
zyadhany <zeyad.hany2003@gmail.com>
2024-09-01 15:50:35 +0300

    paggiation task

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -48,3 +48,33 @@ class Server:
         ind = index_range(page=page, page_size=page_size)
 
         return data[ind[0]: ind[1]]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+            get pages from database csv file
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        data = self.get_page(page=page, page_size=page_size)
+
+        num_pages = (len(self.__dataset) + page_size - 1) // page_size
+
+        if page < num_pages:
+            next_page = page + 1
+        else:
+            next_page = None
+
+        if page > 1:
+            prev_page = page - 1
+        else:
+            prev_page = None
+
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': num_pages
+        }
