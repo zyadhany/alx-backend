96eb27a92367d4e59be4148dbd93a21708dd6de3
zyadhany <zeyad.hany2003@gmail.com>
2024-09-05 20:49:31 +0300

    cashed

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,12 +7,22 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """
+    basic cach
+    """
+
     def put(self, key, item):
+        """
+        add ket to cach
+        """
         if not key or not item:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """
+        get key from cach
+        """
         if not key or key not in self.cache_data:
             return None
         return self.cache_data[key]
