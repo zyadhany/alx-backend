c0d760ca080572d671aa085c22eac0b9cb596fe1
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-10 10:28:59 +0000

    task 3 translate

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+simple Flask
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+
+class Config:
+    """
+    Config class
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    get locale
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """
+    return string
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000, debug=True)
